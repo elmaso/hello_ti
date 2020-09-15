af8fd389afcebbe4ce59073872141e9835ceb8bb
Alejandro Perez <alpe@blitzsoftware.com.mx>
2020-09-15 11:31:22 -0700

    Primer modificacion al PP Flutter 0001

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:hello_ti/constants.dart';
+import 'package:hello_ti/screens/home/home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        //TODO Extraer el color primario de la clase constantes
-        primarySwatch: Colors.teal,
+        primarySwatch: kPrimaryColor,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      //TODO Separa nuestro home en su propia clase dentro de Screns
-      home: MyHomePage(title: 'Telnor TI Home Page'),
-    );
-  }
-}
-
-//TODO Cambiar la clase inicial en un Statless widgets que tenga 
-// Una imagen y un Texto
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ), 
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
+      home: Home(),
     );
   }
 }
